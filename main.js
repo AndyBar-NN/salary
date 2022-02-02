69ac287cbac26d47b234a1ce0c231c448c3fcdd7
AndyBar <andreysparrow91@rambler.ru>
2022-02-03 00:42:43 +0300

    добавлены новые формулы

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ let formControl = document.querySelector('.form_control');
 formControl.addEventListener('click', (e) => {
   e.preventDefault();
   let betProg = 250;
-  let betContent = 156;
   let quantityDays = document.querySelector('.quantity_days').value;
+  let betContent = 30000 / (quantityDays * 8);
+
   quantityDays = parseInt(quantityDays);
   let timeProg = document.querySelector('.time_prog').value;
   timeProg = parseInt(timeProg);
 
   let salarySum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent);
 
-  let salary = document.querySelector('.salary').innerHTML = salarySum;
+  let betContentSum = document.querySelector('.bet_content-sum').innerHTML = Math.round(betContent);
+
+  let salary = document.querySelector('.salary').innerHTML = Math.round(salarySum);
 });
