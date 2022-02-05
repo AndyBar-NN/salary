ee8905a361887f8827502027c6021d4e3abe29ce
AndyBar <andreysparrow91@rambler.ru>
2022-02-06 00:29:24 +0300

    добавлено условие в скрипте

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 let formControl = document.querySelector('.form_control');
+let betContentSum = document.querySelector('.bet_content-sum');
+let salary = document.querySelector('.salary');
+
 formControl.addEventListener('click', (e) => {
   e.preventDefault();
   let betProg = 250;
-  let quantityDays = document.querySelector('.quantity_days').value;
-  let betContent = 30000 / (quantityDays * 8);
 
+  let quantityDays = document.querySelector('.quantity_days').value;
   quantityDays = parseInt(quantityDays);
+
   let timeProg = document.querySelector('.time_prog').value;
   timeProg = parseInt(timeProg);
 
-  let salarySum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent);
+  if (quantityDays && timeProg) {
+    let betContent = 30000 / (quantityDays * 8);
+    let salarySum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent);
 
-  let betContentSum = document.querySelector('.bet_content-sum').innerHTML = Math.round(betContent);
+    betContentSum.innerHTML = Math.round(betContent);
+    salary.innerHTML = Math.round(salarySum);
+  } else {
+    alert("Введите корректные данные");
+  }
 
-  let salary = document.querySelector('.salary').innerHTML = Math.round(salarySum);
 });
