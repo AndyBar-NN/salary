89a209d6de6097136a48a6fad6a17e09b1b0b026
AndyBar <andreysparrow91@rambler.ru>
2022-02-14 19:12:40 +0300

    добавлено поле вклад

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let formControl = document.querySelector('.form_control');
 let betContentSum = document.querySelector('.bet_content-sum');
 let salary = document.querySelector('.salary');
+let depositSum = document.querySelector('.deposit');
 
 formControl.addEventListener('click', (e) => {
   e.preventDefault();
@@ -15,9 +16,11 @@ formControl.addEventListener('click', (e) => {
   if (quantityDays && timeProg) {
     let betContent = 30000 / (quantityDays * 8);
     let salarySum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent);
+    let deposit = salarySum / 10;
 
     betContentSum.innerHTML = Math.round(betContent);
-    salary.innerHTML = Math.round(salarySum);
+    depositSum.innerHTML = Math.round(deposit);
+    salary.innerHTML = Math.round(salarySum) + " ₽";
   } else {
     alert("Введите корректные данные");
   }
